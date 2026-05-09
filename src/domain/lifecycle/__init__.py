68243fb6685cd82e62be99b40955044b20f7ea61
sankar Ramamoorthy <43016553+sankar-ramamoorthy@users.noreply.github.com>
2026-05-09 18:50:48 -0400

    Implement TF-0011 lifecycle state model

---
+++ src/domain/lifecycle/__init__.py
@@ -0,0 +1,15 @@
+from src.domain.lifecycle.state import (
+    CANONICAL_LIFECYCLE_STAGES,
+    LIFECYCLE_EVENT_STAGE_MAP,
+    DecisionLifecycleState,
+    LifecycleStage,
+    derive_lifecycle_state,
+)
+
+__all__ = [
+    "CANONICAL_LIFECYCLE_STAGES",
+    "LIFECYCLE_EVENT_STAGE_MAP",
+    "DecisionLifecycleState",
+    "LifecycleStage",
+    "derive_lifecycle_state",
+]
